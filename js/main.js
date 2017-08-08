20bdb499953befb83517a520e7dba30b49a15ef1
brncolucci <brunocolucci03@hotmail.com>
2017-08-08 12:21:19 -0300

    teste abril

---
+++ js/main.js
@@ -0,0 +1,13 @@
+﻿jQuery(function ($) {
+    $('.menu-ico').click(function () {
+        $('.responsive-menu').toggleClass('expand');
+    })
+})
+
+$(window).bind('scroll', function () {
+    if ($(window).scrollTop() > 0.5) {
+        $('nav').addClass('fixed');
+    } else {
+        $('nav').removeClass('fixed');
+    }
+});
